28308ca4877d1fd284c35372b622e020fdb26ac4
Abhishek Mandal <mandalabhishek8967@gmail.com>
2025-02-09 18:31:05 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,6 @@
+import Image from "next/image";
+
+export default function Home() {
+  
+  return <main className="h-dvh w-full flex items-center justify-center text-2xl">I am Abhishek Mandal.</main>
+}
